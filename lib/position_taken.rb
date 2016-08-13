ed2a76eeec3a918304a3923c42329742eeb37445
Kate Sundberg <katehsundberg@gmail.com>
2016-08-13 16:25:29 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,3 +1,10 @@
 def position_taken?(board, index)
-  board[index] != " "
+  if board[index] == "X" ||
+    board[index] == "O"
+    return true
+  elsif board[index] != " " ||
+    board[index] != "" ||
+    board[index] != nil
+    return false
+  end
 end
